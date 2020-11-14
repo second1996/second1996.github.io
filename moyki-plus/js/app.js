80721a3010babaaf2ced29e37f874551d8018c95
second1996 <vital10012@gmail.com>
2020-11-14 18:38:24 +0200

    Update

--- moyki-plus/js/app.js
+++ moyki-plus/js/app.js
@@ -471,50 +471,145 @@ jQuery(document).ready( function($) {
 	/**
 	 * Quickview Product Modal slider
 	 */
-	var quickviewNavSlider = new Swiper('.quickview-modal .quickview-nav-slider', {
-		observer: true,
-		observeParents: true,
-		allowTouchMove: false,
-		loop: false,
-		slidesPerView: 'auto'
-	})
-	var quickviewBodySlider = new Swiper('.quickview-modal .quickview-body-slider', {
-		observer: true,
-		observeParents: true,
-		allowTouchMove: false,
-		loop: false,
-		speed: 450,
-		slidesPerView: 1,
-		spaceBetween: 30,
-		thumbs: {
-			swiper: quickviewNavSlider
-		}
-	})
-	var quickviewPhotoThumbsSlider = new Swiper('.quickview-modal .quickview-photo-thumbs-slider', {
-		observer: true,
-		observeParents: true,
-		allowTouchMove: false,
-		loop: false,
-		slidesPerView: 'auto',
-		spaceBetween: 8
-	})
-	var quickviewPhotoSlider = new Swiper('.quickview-modal .quickview-photo-slider', {
-		observer: true,
-		observeParents: true,
-		loop: false,
-		speed: 450,
-		slidesPerView: 1,
-		navigation: {
-			nextEl: '.quickview-photo-slider .swiper-button-next',
-			prevEl: '.quickview-photo-slider .swiper-button-prev',
-		},
-		thumbs: {
-			swiper: quickviewPhotoThumbsSlider
-		}
-	})
+	initQuickviewModal = function () {
+		var quickviewNavSlider = new Swiper('.quickview-modal .quickview-nav-slider', {
+			observer: true,
+			observeParents: true,
+			allowTouchMove: false,
+			loop: false,
+			slidesPerView: 'auto'
+		})
+		var quickviewBodySlider = new Swiper('.quickview-modal .quickview-body-slider', {
+			observer: true,
+			observeParents: true,
+			allowTouchMove: false,
+			loop: false,
+			speed: 450,
+			slidesPerView: 1,
+			spaceBetween: 30,
+			thumbs: {
+				swiper: quickviewNavSlider
+			}
+		})
+		var quickviewPhotoThumbsSlider = new Swiper('.quickview-modal .quickview-photo-thumbs-slider', {
+			observer: true,
+			observeParents: true,
+			allowTouchMove: false,
+			loop: false,
+			slidesPerView: 'auto',
+			spaceBetween: 8
+		})
+		var quickviewPhotoSlider = new Swiper('.quickview-modal .quickview-photo-slider', {
+			observer: true,
+			observeParents: true,
+			loop: false,
+			speed: 450,
+			slidesPerView: 1,
+			navigation: {
+				nextEl: '.quickview-photo-slider .swiper-button-next',
+				prevEl: '.quickview-photo-slider .swiper-button-prev',
+			},
+			thumbs: {
+				swiper: quickviewPhotoThumbsSlider
+			}
+		})
+	}
+	initQuickviewModal()
 
 })
 
+jQuery(document).ready( function($) {
+	/**
+	 *-------------------------------------------------------------------------------------------------------------------------------------------
+	 * WIDGET: Price Filter (noUiSlider)
+	 *-------------------------------------------------------------------------------------------------------------------------------------------
+	 */
+	var filterSlider = document.querySelectorAll('.filter-slider-handle');
+
+	if( filterSlider.length ) {
+		initFilterSlider = function() {
+			filterSlider.forEach(slider => {
+				let filterMinPrice = parseInt( slider.dataset.slidermin );
+				let filterMaxPrice = parseInt( slider.dataset.slidermax );
+				let priceFrom      = slider.nextElementSibling.querySelector('.filter-slider-from');
+				let priceTo        = slider.nextElementSibling.querySelector('.filter-slider-to');
+				let inputMinPrice  = parseInt( priceFrom.value );
+				let inputMaxPrice  = parseInt( priceTo.value );
+
+				// Create noUiSlide
+				noUiSlider.create(slider, {
+					start: [inputMinPrice, inputMaxPrice],
+					connect: true,
+					range: {
+							'min': filterMinPrice,
+							'max': filterMaxPrice
+					}
+				})
+
+				// Change/Update noUiSlide values
+				priceFrom.addEventListener('change', priceUpdateValues);
+				priceTo.addEventListener('change', priceUpdateValues);
+
+				slider.noUiSlider.on('update', function (values, handle) {
+					var value = values[handle];
+
+					// Set «from» value
+					if (handle == 0) {
+						priceFrom.value = Math.round(value);
+					}
+
+					// Set «to» value
+					if( handle == 1) {
+						priceTo.value = Math.round(value);
+					}
+				})
+
+				slider.noUiSlider.on('end', function (values, handle) {
+					// Trigger «from» input value
+					if (handle == 0) {
+						$(priceFrom).change()
+					}
+
+					// Trigger «to» input value
+					if( handle == 1) {
+						$(priceTo).change()
+					}
+				})
+
+				function priceUpdateValues() {
+					var priceFromValue, priceToValue;
+
+					// Check if value isn't empty...
+					if (priceFrom.value != '') {
+						priceFromValue = priceFrom.value;
+					}
+					if (priceTo.value != '') {
+						priceToValue = priceTo.value;
+					}
+
+					// ... and change values
+					slider.noUiSlider.set([priceFromValue, priceToValue]);
+				}
+
+				// Set default values when form is reset
+				document.querySelector('.filter-reset').addEventListener('click', function () {
+					slider.noUiSlider.set([parseInt(slider.dataset.slidermin), parseInt(slider.dataset.slidermax)]);
+					priceFrom.setAttribute('value', parseInt(slider.dataset.slidermin));
+					priceTo.setAttribute('value', parseInt(slider.dataset.slidermax));
+				})
+			})
+		}
+		initFilterSlider()
+	}
+
+	// Reinit filter
+	reinitFilterSlider = function() {
+		$('.filter-toggle-header._toggled, .widget-header._toggled').next().css('display', 'block');
+
+		filterSlider = document.querySelectorAll('.filter-slider-handle');
+		initFilterSlider()
+	}
+})
 jQuery(document).ready( function($) {
 
 	/**
--- moyki-plus/js/shop-scripts.js
+++ moyki-plus/js/shop-scripts.js
@@ -125,96 +125,96 @@ document.addEventListener("DOMContentLoaded", function() {
 	})
 
 
-	/**
-	 *-------------------------------------------------------------------------------------------------------------------------------------------
-	 * WIDGET: Price Filter (noUiSlider)
-	 *-------------------------------------------------------------------------------------------------------------------------------------------
-	 */
-	var filterSlider = document.querySelectorAll('.filter-slider-handle');
+	// /**
+	//  *-------------------------------------------------------------------------------------------------------------------------------------------
+	//  * WIDGET: Price Filter (noUiSlider)
+	//  *-------------------------------------------------------------------------------------------------------------------------------------------
+	//  */
+	// var filterSlider = document.querySelectorAll('.filter-slider-handle');
 
-	if( filterSlider.length ) {
-		initFilterSlider = function() {
-			filterSlider.forEach(slider => {
-				let filterMinPrice = parseInt( slider.dataset.slidermin );
-				let filterMaxPrice = parseInt( slider.dataset.slidermax );
-				let priceFrom      = slider.nextElementSibling.querySelector('.filter-slider-from');
-				let priceTo        = slider.nextElementSibling.querySelector('.filter-slider-to');
-				let inputMinPrice  = parseInt( priceFrom.value );
-				let inputMaxPrice  = parseInt( priceTo.value );
+	// if( filterSlider.length ) {
+	// 	initFilterSlider = function() {
+	// 		filterSlider.forEach(slider => {
+	// 			let filterMinPrice = parseInt( slider.dataset.slidermin );
+	// 			let filterMaxPrice = parseInt( slider.dataset.slidermax );
+	// 			let priceFrom      = slider.nextElementSibling.querySelector('.filter-slider-from');
+	// 			let priceTo        = slider.nextElementSibling.querySelector('.filter-slider-to');
+	// 			let inputMinPrice  = parseInt( priceFrom.value );
+	// 			let inputMaxPrice  = parseInt( priceTo.value );
 
-				// Create noUiSlide
-				noUiSlider.create(slider, {
-					start: [inputMinPrice, inputMaxPrice],
-					connect: true,
-					range: {
-							'min': filterMinPrice,
-							'max': filterMaxPrice
-					}
-				})
+	// 			// Create noUiSlide
+	// 			noUiSlider.create(slider, {
+	// 				start: [inputMinPrice, inputMaxPrice],
+	// 				connect: true,
+	// 				range: {
+	// 						'min': filterMinPrice,
+	// 						'max': filterMaxPrice
+	// 				}
+	// 			})
 
-				// Change/Update noUiSlide values
-				priceFrom.addEventListener('change', priceUpdateValues);
-				priceTo.addEventListener('change', priceUpdateValues);
+	// 			// Change/Update noUiSlide values
+	// 			priceFrom.addEventListener('change', priceUpdateValues);
+	// 			priceTo.addEventListener('change', priceUpdateValues);
 
-				slider.noUiSlider.on('update', function (values, handle) {
-					var value = values[handle];
+	// 			slider.noUiSlider.on('update', function (values, handle) {
+	// 				var value = values[handle];
 
-					// Set «from» value
-					if (handle == 0) {
-						priceFrom.value = Math.round(value);
-					}
+	// 				// Set «from» value
+	// 				if (handle == 0) {
+	// 					priceFrom.value = Math.round(value);
+	// 				}
 
-					// Set «to» value
-					if( handle == 1) {
-						priceTo.value = Math.round(value);
-					}
-				})
+	// 				// Set «to» value
+	// 				if( handle == 1) {
+	// 					priceTo.value = Math.round(value);
+	// 				}
+	// 			})
 
-				slider.noUiSlider.on('end', function (values, handle) {
-					// Trigger «from» input value
-					if (handle == 0) {
-						$(priceFrom).change()
-					}
+	// 			slider.noUiSlider.on('end', function (values, handle) {
+	// 				// Trigger «from» input value
+	// 				if (handle == 0) {
+	// 					$(priceFrom).change()
+	// 				}
 
-					// Trigger «to» input value
-					if( handle == 1) {
-						$(priceTo).change()
-					}
-				})
+	// 				// Trigger «to» input value
+	// 				if( handle == 1) {
+	// 					$(priceTo).change()
+	// 				}
+	// 			})
 
-				function priceUpdateValues() {
-					var priceFromValue, priceToValue;
+	// 			function priceUpdateValues() {
+	// 				var priceFromValue, priceToValue;
 
-					// Check if value isn't empty...
-					if (priceFrom.value != '') {
-						priceFromValue = priceFrom.value;
-					}
-					if (priceTo.value != '') {
-						priceToValue = priceTo.value;
-					}
+	// 				// Check if value isn't empty...
+	// 				if (priceFrom.value != '') {
+	// 					priceFromValue = priceFrom.value;
+	// 				}
+	// 				if (priceTo.value != '') {
+	// 					priceToValue = priceTo.value;
+	// 				}
 
-					// ... and change values
-					slider.noUiSlider.set([priceFromValue, priceToValue]);
-				}
+	// 				// ... and change values
+	// 				slider.noUiSlider.set([priceFromValue, priceToValue]);
+	// 			}
 
-				// Set default values when form is reset
-				document.querySelector('.filter-reset').addEventListener('click', function () {
-					slider.noUiSlider.set([parseInt(slider.dataset.slidermin), parseInt(slider.dataset.slidermax)]);
-					priceFrom.setAttribute('value', parseInt(slider.dataset.slidermin));
-					priceTo.setAttribute('value', parseInt(slider.dataset.slidermax));
-				})
-			})
-		}
-		initFilterSlider()
-	}
+	// 			// Set default values when form is reset
+	// 			document.querySelector('.filter-reset').addEventListener('click', function () {
+	// 				slider.noUiSlider.set([parseInt(slider.dataset.slidermin), parseInt(slider.dataset.slidermax)]);
+	// 				priceFrom.setAttribute('value', parseInt(slider.dataset.slidermin));
+	// 				priceTo.setAttribute('value', parseInt(slider.dataset.slidermax));
+	// 			})
+	// 		})
+	// 	}
+	// 	initFilterSlider()
+	// }
 
-	// Reinit filter
-	reinitFilterSlider = function() {
-		$('.filter-toggle-header._toggled, .widget-header._toggled').next().css('display', 'block');
+	// // Reinit filter
+	// reinitFilterSlider = function() {
+	// 	$('.filter-toggle-header._toggled, .widget-header._toggled').next().css('display', 'block');
 
-		filterSlider = document.querySelectorAll('.filter-slider-handle');
-		initFilterSlider()
-	}
+	// 	filterSlider = document.querySelectorAll('.filter-slider-handle');
+	// 	initFilterSlider()
+	// }
 
 	/**
 	 *-------------------------------------------------------------------------------------------------------------------------------------------
