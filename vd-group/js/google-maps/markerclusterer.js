8cdc00232ee46f5ae8e87e6b0a5859bd944b0376
second1996 <vital10012@gmail.com>
2022-12-23 00:39:21 +0200

    Update

--- vd-group/js/google-maps/markerclusterer.js
+++ vd-group/js/google-maps/markerclusterer.js
@@ -639,28 +639,28 @@ function defaultClusterOnAdd(clusterIcon) {
   panes.overlayMouseTarget.appendChild(clusterIcon.div_);
   var isDragging = false;
   var isMouseDown = false;
-  google.maps.event.addDomListener(clusterIcon.div_, 'click', function (event) {
+  clusterIcon.div_.addEventListener('click', function (event) {
     if (!isDragging) {
       clusterIcon.triggerClusterClick(event);
     }
   });
-  google.maps.event.addDomListener(clusterIcon.div_, 'mousedown', function () {
+  clusterIcon.div_.addEventListener('mousedown', function () {
     isDragging = false;
     isMouseDown = true;
   });
-  google.maps.event.addDomListener(clusterIcon.div_, 'mouseup', function () {
+  clusterIcon.div_.addEventListener('mouseup', function () {
     isDragging = false;
     isMouseDown = false;
   });
-  google.maps.event.addDomListener(clusterIcon.div_, 'mousemove', function () {
+  clusterIcon.div_.addEventListener('mousemove', function () {
     if (isMouseDown) {
       isDragging = true;
     }
   });
-  google.maps.event.addDomListener(clusterIcon.div_, 'mouseover', function (event) {
+  clusterIcon.div_.addEventListener('mouseover', function (event) {
     clusterIcon.triggerClusterMouseover(event);
   });
-  google.maps.event.addDomListener(clusterIcon.div_, 'mouseout', function (event) {
+  clusterIcon.div_.addEventListener('mouseout', function (event) {
     clusterIcon.triggerClusterMouseout(event);
   });
 }
--- vd-group/js/google-maps/vd-map.js
+++ vd-group/js/google-maps/vd-map.js
@@ -1,7 +1,8 @@
 var mapElement = document.getElementById('vd-group-map');
-var map, marker, infoWindow;
+var map, marker, infoWindow, markerCluster;
 var markersArr = [];
 
+// Init map function
 function initMap() {
 	if (mapElement) {
 		map = new google.maps.Map(mapElement, {
@@ -10,9 +11,10 @@ function initMap() {
 				lat: 48.9117731,
 				lng: 24.717129,
 			},
-			zoom: 12,
+			zoom: 16,
 			disableDefaultUI: true,
 			zoomControl: true,
+			clickableIcons: false,
 		});
 		setMarkers(map, locations);
 	
@@ -23,8 +25,8 @@ function initMap() {
 	}
 }
 
+// Set marker on the map function
 function setMarkers(map, locations) {
-	var markerEl = './images/marker.svg';
 	map.markers = [];
 
 	for (var locationName in locations) {
@@ -32,11 +34,13 @@ function setMarkers(map, locations) {
 		var pos = new google.maps.LatLng(location.lat, location.lng); // get position object
 
 		// create marker
-		var marker = new google.maps.marker.AdvancedMarkerView({
+		var marker = new google.maps.Marker({
 			map: map,
 			position: pos,
-			title: locationName,
-			content: buildContent(location),
+			category: location.category,
+			title: location.object,
+			icon: location.icon,
+			// content: buildContent(location),
 		});
 
 		map.markers.push(marker); // add marker on Google
@@ -55,54 +59,123 @@ function setMarkers(map, locations) {
 		addInfoWindow(marker, location); // render InfoWindow for marker
 	}
 
-	function buildContent(location) {
-		var content = document.createElement('div');
-
-		content.classList.add('vd-gm-marker');
-		content.innerHTML = `
-			<div class="icon">
-				<img src="${location.icon}" title="${location.object}">
-			</div>
-			<h5 class="title">${location.object}</h5>
-			`;
-
-		return content;
+	// if cluster was previously created
+	if (markerCluster !== undefined) {
+		markerCluster.clearMarkers(); // clear markers from cluster
+		infoWindow.close();
 	}
 
-	function addInfoWindow(marker, body) {
-		infoWindow = new google.maps.InfoWindow();
-		google.maps.event.addListener(marker, 'click', function () {
-			infoWindow.setContent(`<div class="vd-gm-tooltip">
-				<div class="link">
-					<a href="${body.url}" target="_blank">
-						<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-							<path fill-rule="evenodd" d="M18.022 3.502H6.491V0H24v17.51h-3.502V5.977L2.476 24 0 21.524 18.022 3.502Z" clip-rule="evenodd"/>
-						</svg>
-					</a>
-				</div>
-				<h4 class="title">${body.object}</h4>
-				<div class="location">${body.address}</div>
-			</div>`);
-			infoWindow.open(map, marker);
-		});
-	}
+	// apply clustering to the map
+	markerCluster = new MarkerClusterer(map, map.markers, {
+		cssClass: 'vd-gm-cluster',
+		zoomOnClick: true,
+		averageCenter: true,
+		gridSize: 90,
+		minimumClusterSize: 2
+	});
 
 	centerMap(map);
 }
 
+// Add InfoWindow for markers function
+function addInfoWindow(marker, body) {
+	infoWindow = new google.maps.InfoWindow();
+	google.maps.event.addListener(marker, 'click', function () {
+		infoWindow.setContent(`<div class="vd-gm-tooltip">
+			<div class="link">
+				<a href="${body.url}" target="_blank">
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+						<path fill-rule="evenodd" d="M18.022 3.502H6.491V0H24v17.51h-3.502V5.977L2.476 24 0 21.524 18.022 3.502Z" clip-rule="evenodd"/>
+					</svg>
+				</a>
+			</div>
+			<h4 class="title">${body.object}</h4>
+			<div class="location">${body.address}</div>
+		</div>`);
+		infoWindow.open(map, marker);
+	});
+}
+
+// Build InfoWindow content function
+function buildContent(location) {
+	var content = document.createElement('div');
+
+	// render marker icon
+	switch (location.category) {
+		case 'park':
+			content.classList.add('vd-gm-marker-park');
+			content.innerHTML = `
+				<div class="icon">
+					<img src="${location.icon}" title="${location.object}">
+				</div>
+				`;
+			break;
+		case 'university':
+			content.classList.add('vd-gm-marker-university');
+			content.innerHTML = `
+				<div class="icon">
+					<img src="${location.icon}" title="${location.object}">
+				</div>
+				`;
+			break;
+		case 'kindergarten':
+			content.classList.add('vd-gm-marker-kindergarten');
+			content.innerHTML = `
+				<div class="icon">
+					<img src="${location.icon}" title="${location.object}">
+				</div>
+				`;
+			break;
+		default:
+			content.classList.add('vd-gm-marker');
+			content.innerHTML = `
+				<div class="icon">
+					<img src="${location.icon}" title="${location.object}">
+				</div>
+				<h5 class="title">${location.object}</h5>
+				`;
+			break;
+	}
+
+	return content;
+}
+
+// Filtering markers function
+function filterMarkers(category) {
+	var filteredLocations;
+
+	// filtering markers by category
+	if (category !== 'all') {
+		filteredLocations = locations.filter(function (location) {
+			return location.category === category;
+		});
+	} else {
+		filteredLocations = locations;
+	}
+
+	// show/hide marker on the map 
+	markersArr.forEach(function (marker) {
+		if (!(marker.category === 'house' || marker.category === 'office')) {
+			marker.setMap(null);
+		}
+	});
+
+	// set filtered markers
+	setMarkers(map, filteredLocations);
+}
+
+// Centering map (bounds) function
 function centerMap(map) {
 	var bounds = new google.maps.LatLngBounds();
 
-	map.markers.forEach(function (marker, i) {
-		var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
-		bounds.extend(latlng);
+	map.markers.forEach(function (marker) {
+		bounds.extend(new google.maps.LatLng(marker.getPosition()));
 	});
 
 	if (map.markers.length === 1) {
 		map.setCenter(bounds.getCenter());
 		map.setZoom(16);
 	} else {
-		// fit to bounds
 		map.fitBounds(bounds);
 	}
 }
@@ -110,8 +183,8 @@ function centerMap(map) {
 // Pan to marker by clicking on an Object card
 var objectCards = document.querySelectorAll('.objects .object .btn-inline-arrow');
 
-objectCards.forEach(function(object) {
-	object.addEventListener('click', function(event) {
+objectCards.forEach(function (object) {
+	object.addEventListener('click', function (event) {
 		var objectLat = event.currentTarget.dataset.lat;
 		var objectLng = event.currentTarget.dataset.lng;
 
@@ -125,7 +198,7 @@ objectCards.forEach(function(object) {
 
 				// if lng and lat is the same with marker
 				map.panTo(latLng); // move map to marker
-				map.setZoom(18); // zoom map
+				map.setZoom(16); // zoom map
 				window.scrollTo({
 					top: document.querySelector('.objects').offsetTop + 60,
 					behavior: 'smooth'
@@ -134,5 +207,23 @@ objectCards.forEach(function(object) {
 				infoWindow.close();
 			}
 		}
+	});
+});
+
+// Filtering buttons (Location section Single page project)
+var filterBtns = document.querySelectorAll('.location-filters .location-filters-btn');
+
+filterBtns.forEach(function (button) {
+	button.addEventListener('click', function (event) {
+		var _this = event.currentTarget;
+		var category = _this.dataset.category;
+
+		for (const btn of filterBtns) {
+			btn.classList.remove('_is-active');
+		}
+
+		_this.classList.add('_is-active');
+		infoWindow.close();
+		filterMarkers(category);
 	});
 });
